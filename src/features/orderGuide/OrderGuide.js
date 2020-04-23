7001e923cb3e51dc9a8e3e6de073177bf382fba6
Thomas Herzog <Tomrule007@gmail.com>
2020-04-22 20:08:41 -0700

    Add mockdata

--- src/features/orderGuide/OrderGuide.js
+++ src/features/orderGuide/OrderGuide.js
@@ -10,53 +10,51 @@ import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 
 const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString(),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString(),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
-  },
+  { id: 'brand', label: 'Brand' },
+  { id: 'upc', label: 'UPC' },
+  { id: 'description', label: 'Description' },
+  { id: 'retail', label: 'Retail' },
+  { id: 'caseRetail', label: 'Case Retail' },
 ];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
+function createData([brand, upc, description, retail, caseCost]) {
+  const caseRetail = (caseCost * 1.25).toFixed(2);
+  return { brand, upc, description, retail, caseRetail };
 }
 
 const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
+  ['WA', 94178, 'APPLE "LUNCHBOX" SMALL SIZE MISC', 1.91, 36.86],
+  ['WA', 93616, 'APPLE ENVY OG 27#', 2.57, 45.41],
+  ['WA', 93231, 'APPLE ENVY OG 38# "LUNCHBOX"', 1.14, 29.76],
+  ['ORWA', 94131, 'APPLE FUJI OG', 2.44, 30.67],
+  ['ORWA', 93229, 'APPLE FUJI OG 38# "LUNCHBOX"', 2.01, 47.91],
+  ['ORWA', 94174, 'APPLE GALA OG 38#', 3.3, 33.85],
+  ['CAWA', 94017, 'APPLE GRANNY SMITH OG', 2.69, 37.37],
+  ['WA', 93232, 'APPLE HONEYCRISP OG  "LUNCHBOX', 2.01, 44.41],
+  ['ORWA', 93283, 'APPLE HONEYCRISP OG 38#', 4.51, 45.57],
+  ['WA', 94115, 'APPLE JAZZ OG', 2.22, 45.32],
+  ['WA', 93618, 'APPLE OPAL OG', 2.79, 26.26],
+  ['WA', 94130, 'APPLE PINK LADY OG 38#', 2.16, 49.54],
+  ['NEWZE', 94210, 'APPLE PREMIER STAR OG', 1.81, 32.58],
+  ['CHILE', 3603, 'APPLE TANGO UNWAXED "NEWCROP"', 3.36, 40.21],
+  ['CA', 94218, 'APRICOT OG 2LAYER', 4.72, 24.78],
+  ['CA', 94221, 'AVOCADO FUERTE OG 48CT', 5.56, 66.95],
+  ['MEX', 76101098146, 'AVOCADO HASS BAG OG MINI 14X6', 4.32, 52.25],
+  ['MEX', 7074014570, 'AVOCADO HASS BAGS OG', 4.22, 40.32],
+  ['CA', 94228, 'AVOCADO HASS BAGS OG 18X4CT', 5.56, 48.16],
+  ['MEX', 4221, 'AVOCADO HASS NOG  #2 48CT/LG VA ONLY', 2.31, 45.04],
+  ['CA', 94046, 'AVOCADO HASS OG 70CT', 2.91, 40.02],
+  ['CA', 86174700002, 'AVOCADO HASS OG 8X4 GATOR EGGS', 5.65, 31.94],
+  ['CA', 94225, 'AVOCADO HASS OG LARGE 32/40CT', 3.0, 71.34],
+  ['HAWAC', 94771, 'AVOCADO SHARWIL OG 30# WA ONLY', 4.0, 95.41],
+  ['EC', 4234, 'BANANA BABY 15#', 1.97, 14.23],
+  ['EC', 94234, 'BANANA BABY OG 15#', 2.22, 16.61],
+  ['EC', 4235, 'BANANA PLANTAIN 50#', 0.9, 38.69],
+  ['MEX', 94235, 'BANANA PLANTAINS OG 50#', 1.48, 49.72],
+  ['ECM', 94011, 'BANANAS OG', 0.77, 20.04],
+  ['MEX', 94251, 'BERRY BLACKBERRIES OG 6OZ', 4.06, 32.16],
+  ['MEX', 4239, 'BERRY BLACKBERRY NOG 12X12OZ', 4.43, 49.52],
+].map(createData);
 
 const useStyles = makeStyles({
   root: {
