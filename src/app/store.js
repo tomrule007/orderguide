59f1b044996a8715bb6c9498a3e6a82567e25324
Thomas Herzog <Tomrule007@gmail.com>
2020-04-23 00:21:35 -0700

    Create orderGuideSlice with mock initial data

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import orderGuideReducer from '../features/orderGuide/orderGuideSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    orderGuide: orderGuideReducer,
   },
 });
---
+++ src/features/orderGuide/orderGuideSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { data } from './orderGuideMockData';
+
+export const orderGuideSlice = createSlice({
+  name: 'orderGuide',
+  initialState: {
+    data,
+    filterText: '',
+  },
+  reducers: {
+    setFilterText: (state, action) => {
+      state.filterText = String(action.payload).toLowercase();
+    },
+    setData: (state, action) => {
+      state.data = action.payload;
+    },
+  },
+});
+
+export const { setFilterText, setData } = orderGuideSlice.actions;
+
+export default orderGuideSlice.reducer;
