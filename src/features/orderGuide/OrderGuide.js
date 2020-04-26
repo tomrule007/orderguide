ed1e2791982f8b2470a69049cb2e034ce83264c6
Thomas Herzog <Tomrule007@gmail.com>
2020-04-25 21:10:19 -0700

    Revert "Remove pagination from OrderGuide table."
    This reverts commit 6ccada0bae1fa5c2ee4ce57599c8f56e6103c983.

--- src/features/orderGuide/OrderGuide.js
+++ src/features/orderGuide/OrderGuide.js
@@ -1,11 +1,13 @@
 import React, { useMemo, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 
 import { getOrderGuideData } from '../orderGuide/orderGuideSlice';
@@ -52,6 +54,17 @@ export default function OrderGuideTable() {
   useEffect(() => {
     dispatch(getOrderGuideData());
   }, []);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
   const classes = useStyles();
   const data = useSelector((state) => state.orderGuide.data);
   const filterText = useSelector((state) => state.orderGuide.filterText);
@@ -60,42 +73,55 @@ export default function OrderGuideTable() {
     [data, filterText]
   );
   return (
-    <TableContainer className={classes.root}>
-      <Table stickyHeader size="small" aria-label="sticky table">
-        <TableHead>
-          <TableRow>
-            {columns.map((column) => (
-              <TableCell
-                key={column.id}
-                align={column.align}
-                style={{ minWidth: column.minWidth }}
-              >
-                {column.label}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {filteredData.map((row) => {
-            return (
-              <TableRow hover role="checkbox" tabIndex={-1} key={row.upc}>
-                {columns.map((column) => {
-                  const value = row[column.id];
-                  return (
-                    <TableCell key={column.id} align={column.align}>
-                      <>
-                        {column.format
-                          ? column.format(filterText, value)
-                          : value}
-                      </>
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <TableContainer className={classes.root}>
+        <Table stickyHeader size="small" aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredData
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.upc}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          <>
+                            {column.format
+                              ? column.format(filterText, value)
+                              : value}
+                          </>
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={filteredData.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </>
   );
 }
