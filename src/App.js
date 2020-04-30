bc616b73fddd8978e363a326bb5eda0980fa3341
Thomas Herzog <Tomrule007@gmail.com>
2020-04-30 15:55:45 -0700

    Lots of changes. > App compoent loads orderguide state > If loading orderguide show spinner > else if no data show FileLoader button > else show OrderGuide.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from './features/appBar/AppBar';
 import AppDrawer from './features/appDrawer/AppDrawer';
 import OrderGuide from './features/orderGuide/OrderGuide';
+import FileLoader from './features/fileLoader/FileLoader';
+
+import { getOrderGuideData } from './features/orderGuide/orderGuideSlice';
+
+const useStyles = makeStyles({
+  app: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100vh',
+  },
+  center: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: '1 1 auto',
+  },
+});
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => dispatch(getOrderGuideData()), []);
+  const { data, filterText } = useSelector((state) => state.orderGuide);
+  const isLoading = useSelector((state) => state.fileLoader.isLoading);
+
+  const classes = useStyles();
   return (
-    <>
-      <AppBar />
+    <div className={classes.app}>
       <AppDrawer />
-      <OrderGuide />
-    </>
+      <AppBar />
+      {isLoading ? (
+        <div className={classes.center}>
+          <CircularProgress size="5rem" />
+        </div>
+      ) : data.length ? (
+        <div>
+          <OrderGuide data={data} filterText={filterText} />
+        </div>
+      ) : (
+        <div className={classes.center}>
+          <FileLoader />
+        </div>
+      )}
+    </div>
   );
 }
 
