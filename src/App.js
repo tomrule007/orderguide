e455b0c83236670f7dfcbd2aff24df3314b1a243
Thomas Herzog <Tomrule007@gmail.com>
2020-04-30 14:27:46 -0700

    Enable AppDrawer and remove FileLoader button.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import AppBar from './features/appBar/AppBar';
 import AppDrawer from './features/appDrawer/AppDrawer';
 import OrderGuide from './features/orderGuide/OrderGuide';
-import FileLoader from './features/fileLoader/FileLoader';
+
 function App() {
   return (
     <>
       <AppBar />
-      {/* <AppDrawer /> */}
-      <FileLoader />
+      <AppDrawer />
       <OrderGuide />
     </>
   );
