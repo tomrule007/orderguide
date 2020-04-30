59b043994382eadbe386d7698b5fba41004e6e09
Thomas Herzog <Tomrule007@gmail.com>
2020-04-30 14:26:35 -0700

    Add FileLoad button to AppDrawer.
    Also:
    > Remove sample list items.
    > Wire toggleDrawer action to AppDrawer.

--- src/features/appDrawer/AppDrawer.js
+++ src/features/appDrawer/AppDrawer.js
@@ -1,35 +1,18 @@
 import React from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
+import { useSelector, useDispatch } from 'react-redux';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 
-const useStyles = makeStyles({
-  list: {
-    width: 250,
-  },
-  fullList: {
-    width: 'auto',
-  },
-});
+import FileLoader from '../fileLoader/FileLoader';
 
-export default function SwipeableTemporaryDrawer() {
-  const classes = useStyles();
-  const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
+import { toggleDrawer } from './appDrawerSlice';
 
-  const toggleDrawer = (anchor, open) => (event) => {
+export default function AppDrawer() {
+  const dispatch = useDispatch();
+  const isOpen = useSelector((state) => state.appDrawer.isOpen);
+
+  const handleToggleDrawer = (open) => (event) => {
     if (
       event &&
       event.type === 'keydown' &&
@@ -37,58 +20,21 @@ export default function SwipeableTemporaryDrawer() {
     ) {
       return;
     }
-
-    setState({ ...state, [anchor]: open });
+    dispatch(toggleDrawer(open));
   };
 
-  const list = (anchor) => (
-    <div
-      className={clsx(classes.list, {
-        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
-      })}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
+  return (
+    <SwipeableDrawer
+      anchor="left"
+      open={isOpen}
+      onClose={handleToggleDrawer(false)}
+      onOpen={handleToggleDrawer(true)}
     >
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        <ListItem>
+          <FileLoader />
+        </ListItem>
       </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-
-  return (
-    <div>
-      {['left', 'right', 'top', 'bottom'].map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-          <SwipeableDrawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-            onOpen={toggleDrawer(anchor, true)}
-          >
-            {list(anchor)}
-          </SwipeableDrawer>
-        </React.Fragment>
-      ))}
-    </div>
+    </SwipeableDrawer>
   );
 }
