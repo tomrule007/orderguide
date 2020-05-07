2d48ec92e003ad2ac7d62628a7ee3f4c43d5766f
Thomas Herzog <Tomrule007@gmail.com>
2020-05-07 11:59:46 -0700

    Create disableInstructionModal function
    (also put txt into <p> elements and fixed some typos)

--- src/features/instructionModal/InstructionModal.js
+++ src/features/instructionModal/InstructionModal.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const InstructionalModal = () => {
-  return (
+  const [hideInstructions, setHideInstructions] = useState(true);
+
+  useEffect(() => {
+    if (localStorage.getItem('hideInstructions')) return;
+    setHideInstructions(false);
+  });
+
+  const disableInstructionModal = () => {
+    localStorage.setItem('hideInstructions', true); //TODO: Handle storage error situations.
+    setHideInstructions(true);
+  };
+  return hideInstructions ? null : (
     <div>
-      Welcome to orderguide.netlify.app This website provides a searchable web
-      interface for the produce departments weekly excel spreadsheet price list.
-      If you dont have acess to the spreadsheet you can test run the website
-      with the included "load mock data" link. Note: this website is also a
-      Progressive web app meaning your browser will ask if you would like to add
-      a shortcut to your desktop. This will allow the app to run in offline mode
-      and also display in fullscreen mode. It will not give the app access to
-      your files and is no less secure than visiting a website.
+      <p> Welcome to orderguide.netlify.app</p>
+      <p>
+        This website provides a searchable web interface for the produce
+        departments weekly excel spreadsheet price list.
+      </p>
+      <p>
+        If you don't have access to the spreadsheet you can test run the website
+        with the included "load mock data" link.
+      </p>
+      <p>
+        Note: this website is also a Progressive web app meaning your browser
+        will ask if you would like to add a shortcut to your desktop. This will
+        allow the app to run in offline mode and also display in fullscreen
+        mode. It will not give the app access to your files and is no less
+        secure than visiting a website.
+      </p>
+      <button onClick={disableInstructionModal}>disable popup</button>
     </div>
   );
 };
