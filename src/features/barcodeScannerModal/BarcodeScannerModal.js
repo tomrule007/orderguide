5503e206cc715e1e2179db3665b0fce7165d969e
Thomas Herzog <Tomrule007@gmail.com>
2020-05-15 09:09:58 -0700

    Cleanup BarcodeScannerModal and remove test button

--- src/features/barcodeScannerModal/BarcodeScannerModal.js
+++ src/features/barcodeScannerModal/BarcodeScannerModal.js
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -19,69 +17,44 @@ import {
 import BarcodeScanner from '../barcodeScanner/BarcodeScanner';
 import './BarcodeScannerModal.css';
 
-const useStyles = makeStyles((theme) => ({
-  title: {
-    marginLeft: theme.spacing(2),
-    flex: 1,
-  },
-}));
-
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
 export default function BarcodeScannerModal() {
-  const classes = useStyles();
-
   const open = useSelector(selectOpen);
-  console.log('open = ', open);
   const enableScanner = useSelector(selectEnableScanner);
-
   const dispatch = useDispatch();
 
-  function handleClickOpen() {
-    console.log('OPEN AND SET SCANNER TO ENABLED MODE');
-    dispatch(setEnableScanner(true));
-    dispatch(setOpen(true));
-  }
-
-  function handleClose() {
-    console.log('CLOSE AND SET SCANNER TO DISABLED MODE');
+  const handleClose = () => {
     dispatch(setEnableScanner(false));
     dispatch(setOpen(false));
-  }
-
+  };
   const onDetected = (results) => {
-    console.log('results', results);
     dispatch(setFilterText(results));
     handleClose();
   };
 
   return (
-    <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open full-screen dialog
-      </Button>
-      <Dialog
-        fullScreen
-        open={open}
-        onClose={handleClose}
-        TransitionComponent={Transition}
-      >
-        <AppBar color="transparent" elevation={0}>
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              onClick={handleClose}
-              aria-label="close"
-            >
-              <CloseIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <BarcodeScanner onDetected={onDetected} enabled={enableScanner} />
-      </Dialog>
-    </div>
+    <Dialog
+      fullScreen
+      open={open}
+      onClose={handleClose}
+      TransitionComponent={Transition}
+    >
+      <AppBar color="transparent" elevation={0}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            onClick={handleClose}
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <BarcodeScanner onDetected={onDetected} enabled={enableScanner} />
+    </Dialog>
   );
 }
