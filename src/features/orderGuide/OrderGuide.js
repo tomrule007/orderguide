cacf3c37311864606daf98bc290fefc48064d422
Thomas Herzog <Tomrule007@gmail.com>
2020-12-02 11:42:39 -0800

    Set 25 rows as default orderguide view

--- src/features/orderGuide/OrderGuide.js
+++ src/features/orderGuide/OrderGuide.js
@@ -68,7 +68,7 @@ const rowIncludes = (filterText) => (row) => {
 
 export default function OrderGuideTable({ data, filterText }) {
   const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [rowsPerPage, setRowsPerPage] = React.useState(25);
   const theme = useTheme();
   const isLargeScreen = useMediaQuery(theme.breakpoints.up('md'));
 
