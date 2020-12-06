6be9216d18daff1df3891f1ac2eb8c785c127ecd
Thomas Herzog <Tomrule007@gmail.com>
2020-12-06 10:18:03 -0800

    Remove counter component references

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from 'components/counter/counterSlice';
 import orderGuideReducer from 'components/ProductTable/orderGuideSlice';
 import fileLoaderReducer from 'components/fileLoader/fileLoaderSlice';
 import appDrawerReducer from 'components/appDrawer/appDrawerSlice';
 import appBarReducer from 'components/appBar/appBarSlice';
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     orderGuide: orderGuideReducer,
     fileLoader: fileLoaderReducer,
     appDrawer: appDrawerReducer,
