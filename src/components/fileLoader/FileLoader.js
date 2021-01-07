2d50eb197e7987a3f6daf3cc35d24bddc251ad64
Thomas Herzog <Tomrule007@gmail.com>
2021-01-07 09:13:17 -0800

    Convert FileLoader to use newer fileStoreSlice

--- src/components/fileLoader/FileLoader.js
+++ src/components/fileLoader/FileLoader.js
@@ -5,7 +5,7 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
-import { loadFile } from '../fileLoader/fileLoaderSlice';
+import { loadFile } from 'reducers/fileStoreSlice';
 
 const useStyles = makeStyles((theme) => ({
   button: {
