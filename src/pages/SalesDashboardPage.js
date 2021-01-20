d962929d52f8536d2137b86766fde3666b54a231
Thomas Herzog <Tomrule007@gmail.com>
2021-01-20 13:43:26 -0800

    Consolidate state to prevent race conditions

--- src/pages/SalesDashboardPage.js
+++ src/pages/SalesDashboardPage.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { makeStyles } from '@material-ui/core/styles';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import ProductTable from 'components/ProductTable/ProductTable';
-import FileLoader from 'components/fileLoader/FileLoader';
-import MockDataLink from 'components/mockDataLink/MockDataLink';
-import { selectFilterText } from 'components/appBar/appBarSlice';
-import { selectDays } from 'reducers/filtersSlice';
-import { selectProductMap } from 'reducers/productMapSlice';
 import {
   getOrderGuide,
   selectOrderGuideMetadata,
 } from 'reducers/fileStoreSlice';
+
+import CircularProgress from '@material-ui/core/CircularProgress';
+import FileLoader from 'components/fileLoader/FileLoader';
+import MockDataLink from 'components/mockDataLink/MockDataLink';
+import ProductTable from 'components/ProductTable/ProductTable';
+import { makeStyles } from '@material-ui/core/styles';
+import { selectDays } from 'reducers/filtersSlice';
+import { selectFilterText } from 'components/appBar/appBarSlice';
+import { selectProductMap } from 'reducers/productMapSlice';
+import { useSelector } from 'react-redux';
 const useStyles = makeStyles({
   center: {
     display: 'flex',
@@ -36,18 +37,18 @@ const SalesDashboardPage = () => {
   const productMap = useSelector(selectProductMap);
   // TODO: copied code from OrderGuidePage (could possibly be extracted to custom hook)
   const orderGuideMetadata = useSelector(selectOrderGuideMetadata);
-  const [orderGuide, setOrderGuide] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [state, setState] = useState({ loading: true, orderGuide: null });
+  const { loading, orderGuide } = state;
+
   useEffect(() => {
-    if (!isLoading) setIsLoading(true);
+    if (!loading) setState({ ...state, loading: true });
     getOrderGuide().then((guide) => {
-      setOrderGuide(guide);
-      setIsLoading(false);
+      setState({ ...state, orderGuide: guide, loading: false });
     });
     return () => {};
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [orderGuideMetadata]);
-  return isLoading ? (
+  return loading ? (
     <div className={classes.center}>
       <CircularProgress size="5rem" />
     </div>
